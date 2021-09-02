fd30af1778beac6fc599645d90ca179343fa45d7
Derek Hurley <dzhurley@gmail.com>
2021-09-01 22:17:22 -0700

    split apart Range from Filters

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,95 +1,8 @@
-import { useCallback, useState } from 'react';
-import { Range, getTrackBackground } from 'react-range';
-import { debounce } from 'lodash';
+import { memo, useCallback } from 'react';
+
+import Range from './Range';
 
 import styles from './Filters.module.css';
-
-const Slider: React.FC<{
-  label: string;
-  min: number;
-  max: number;
-  step: number;
-  value: number[] | null;
-  onChange: (values: number[]) => void;
-  units?: string;
-}> = ({ label, min, max, step, value, onChange, units }) => {
-  const [values, setValues] = useState(value ?? [min, max]);
-
-  /* eslint-disable-next-line react-hooks/exhaustive-deps */
-  const debouncedUpdate = useCallback(debounce(onChange, 200), [onChange]);
-
-  return (
-    <label className={styles.slider}>
-      {label}
-      <Range
-        draggableTrack
-        step={step}
-        min={min}
-        max={max}
-        values={values}
-        onChange={(newValues) => {
-          setValues(newValues);
-          debouncedUpdate(newValues);
-        }}
-        renderTrack={({ props, children }) => (
-          <div
-            onMouseDown={props.onMouseDown}
-            onTouchStart={props.onTouchStart}
-            style={{
-              ...props.style,
-              height: '28px',
-              display: 'flex',
-              width: 'calc(100% - 1rem)',
-              padding: '0 0.5rem',
-            }}
-          >
-            <div
-              ref={props.ref}
-              style={{
-                height: '5px',
-                width: '100%',
-                borderRadius: '4px',
-                background: getTrackBackground({
-                  values,
-                  colors: ['#ccc', '#548BF4', '#ccc'],
-                  min: min,
-                  max: max,
-                }),
-                alignSelf: 'center',
-              }}
-            >
-              {children}
-            </div>
-          </div>
-        )}
-        renderThumb={({ props }) => (
-          <div
-            {...props}
-            style={{
-              ...props.style,
-              height: '14px',
-              width: '14px',
-              borderRadius: '50%',
-              backgroundColor: '#FFF',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              boxShadow: '0px 2px 6px #AAA',
-            }}
-          />
-        )}
-      />
-      <output
-        style={{ fontSize: '80%', textAlign: 'center', display: 'block' }}
-      >
-        {step < 1
-          ? `${values[0].toFixed(1)} - ${values[1].toFixed(1)}`
-          : `${values[0]} - ${values[1]}`}{' '}
-        {units ? `(${units})` : ''}
-      </output>
-    </label>
-  );
-};
 
 const Filters: React.FC<{
   filters: Record<string, any>;
@@ -102,7 +15,7 @@ const Filters: React.FC<{
     [updateFilter],
   );
 
-  const handleSlider = useCallback(
+  const handleRange = useCallback(
     (name: string) => (values: number[]) => {
       updateFilter(name, values);
     },
@@ -139,75 +52,75 @@ const Filters: React.FC<{
 
       <h3>Equity</h3>
 
-      <Slider
+      <Range
         label="Asthma"
         min={5}
         max={20}
         step={0.1}
         value={filters.asthma}
-        onChange={handleSlider('asthma')}
+        onChange={handleRange('asthma')}
         units="%"
       />
 
-      <Slider
+      <Range
         label="Average Temperature"
         min={50}
         max={100}
         step={0.1}
         value={filters.avg_temp}
-        onChange={handleSlider('avg_temp')}
+        onChange={handleRange('avg_temp')}
         units="degrees"
       />
 
-      <Slider
+      <Range
         label="Median Household Income"
         min={0}
         max={250000}
         step={1000}
         value={filters.medhhinc}
-        onChange={handleSlider('medhhinc')}
+        onChange={handleRange('medhhinc')}
         units="$"
       />
 
-      <Slider
+      <Range
         label="Mental Health Issues"
         min={5}
         max={25}
         step={0.1}
         value={filters.ment_hlth}
-        onChange={handleSlider('ment_hlth')}
+        onChange={handleRange('ment_hlth')}
         units="%"
       />
 
-      <Slider
+      <Range
         label="Physical Health Issues"
         min={5}
         max={25}
         step={0.1}
         value={filters.phys_hlth}
-        onChange={handleSlider('phys_hlth')}
+        onChange={handleRange('phys_hlth')}
         units="%"
       />
 
-      <Slider
+      <Range
         label="Population"
         min={500}
         max={5500}
         step={100}
         value={filters.total_pop}
-        onChange={handleSlider('total_pop')}
+        onChange={handleRange('total_pop')}
       />
 
-      <Slider
+      <Range
         label="Tree Equity Score"
         min={30}
         max={100}
         step={0.1}
         value={filters.tes}
-        onChange={handleSlider('tes')}
+        onChange={handleRange('tes')}
       />
     </section>
   );
 };
 
-export default Filters;
+export default memo(Filters);
---
+++ components/Range.tsx
@@ -0,0 +1,74 @@
+import { useCallback, useState } from 'react';
+import { Range as ReactRange, getTrackBackground } from 'react-range';
+import { debounce } from 'lodash';
+
+import styles from './Range.module.css';
+
+const Range: React.FC<{
+  label: string;
+  min: number;
+  max: number;
+  step: number;
+  value: number[] | null;
+  onChange: (values: number[]) => void;
+  units?: string;
+}> = ({ label, min, max, step, value, onChange, units }) => {
+  const [values, setValues] = useState(value ?? [min, max]);
+
+  /* eslint-disable-next-line react-hooks/exhaustive-deps */
+  const debouncedUpdate = useCallback(debounce(onChange, 500), [onChange]);
+
+  return (
+    <label className={styles.range}>
+      {label}
+      <ReactRange
+        draggableTrack
+        step={step}
+        min={min}
+        max={max}
+        values={values}
+        onChange={(newValues) => {
+          setValues(newValues);
+          debouncedUpdate(newValues);
+        }}
+        renderTrack={({ props, children }) => {
+          const background = getTrackBackground({
+            values,
+            colors: ['#ccc', '#548BF4', '#ccc'],
+            min: min,
+            max: max,
+          });
+          return (
+            <div
+              className={styles.trackContainer}
+              onMouseDown={props.onMouseDown}
+              onTouchStart={props.onTouchStart}
+              style={props.style}
+            >
+              <div
+                ref={props.ref}
+                className={styles.track}
+                style={{ background }}
+              >
+                {children}
+              </div>
+            </div>
+          );
+        }}
+        renderThumb={({ props }) => (
+          <div {...props} className={styles.thumb} style={props.style} />
+        )}
+      />
+      <output
+        style={{ fontSize: '80%', textAlign: 'center', display: 'block' }}
+      >
+        {step < 1
+          ? `${values[0].toFixed(1)} - ${values[1].toFixed(1)}`
+          : `${values[0]} - ${values[1]}`}{' '}
+        {units ? `(${units})` : ''}
+      </output>
+    </label>
+  );
+};
+
+export default Range;
