c68ef2dff1a0b5ccd99129a1c997e050a64b11ad
Derek Hurley <dzhurley@gmail.com>
2021-10-05 07:37:51 -0700

    add link to github

--- components/About.tsx
+++ components/About.tsx
@@ -17,31 +17,46 @@ const About: React.FC = () => {
             onClick={() => setOpen(false)}
           ></section>
           <section className={styles.content}>
-            The supporting demo for my talk at{' '}
-            <a
-              href="https://2021.cascadiajs.com/speakers/derek-hurley"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              CascadiaJS
-            </a>{' '}
-            entitled {`"Cartography on the Web"`}. This merges datasets from the{' '}
-            <a
-              href="https://treeequityscore.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Tree Equity Score
-            </a>{' '}
-            and the{' '}
-            <a
-              href="https://www.portland.gov/trees/get-involved/treeinventory"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Portland Tree Inventory Project
-            </a>
-            . Look around and enjoy!
+            <p>
+              The supporting demo for my talk at{' '}
+              <a
+                href="https://2021.cascadiajs.com/speakers/derek-hurley"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                CascadiaJS
+              </a>{' '}
+              entitled {`"Cartography on the Web"`}. This merges datasets from
+              the{' '}
+              <a
+                href="https://treeequityscore.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Tree Equity Score
+              </a>{' '}
+              and the{' '}
+              <a
+                href="https://www.portland.gov/trees/get-involved/treeinventory"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Portland Tree Inventory Project
+              </a>
+              . Look around and enjoy!
+            </p>
+
+            <p>
+              Source can be found on{' '}
+              <a
+                href="https://github.com/dzhurley/treelandia/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+              .
+            </p>
           </section>
         </>
       )}
